6f4069d876b4cc135cf39a6112c32a717e47ec47
raquelmvz <raquel.mvz98@gmail.com>
2021-02-04 09:56:40 +0100

    Crea clase Persona

---
+++ src/main/java/t6araquel/Persona.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t6araquel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author raquel
+ */
+public class Persona {
+    
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    public enum Genero {MUJER, HOMBRE};
+    private Genero genero;
+    
+    //----CONSTRUCTORES--------
+
+    public Persona(String dni, String nombre, String apellidos, Genero genero) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.genero = genero;
+    }
+
+    public Persona() {
+    }
+    
+    //-----GETTERS Y SETTERS--------
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+    
+    //-------HASH CODE & EQUALS---------
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + Objects.hashCode(this.dni);
+        hash = 47 * hash + Objects.hashCode(this.nombre);
+        hash = 47 * hash + Objects.hashCode(this.apellidos);
+        hash = 47 * hash + Objects.hashCode(this.genero);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.dni, other.dni)) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidos, other.apellidos)) {
+            return false;
+        }
+        if (this.genero != other.genero) {
+            return false;
+        }
+        return true;
+    }
+    
+    //-------TO STRING-----------
+
+    @Override
+    public String toString() {
+        return "Persona{" + "dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", genero=" + genero + '}';
+    }
+    
+    
+    
+}
